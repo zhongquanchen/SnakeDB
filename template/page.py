47e0306ef5efcc1572d22c3f7050efbc0bd7c3f7
zhongquan <oscarfen@icloud.com>
2020-02-02 03:35:09 -0800

    implement update with bugs to fix

--- template/page.py
+++ template/page.py
@@ -4,6 +4,7 @@ from template.config import *
 class Page:
 
     def __init__(self):
+        self.location = 0
         self.num_records = 0
         self.data = bytearray(4096)
         self.index_lookup = {}
@@ -13,17 +14,21 @@ class Page:
             return False
         return True
 
-    def write(self, value):
-        value_list = self.convert_8byte(value)
-
-        for i in range(len(value_list)):
-            self.data[self.num_records * 8 + i] = value_list[i]
-
-        self.num_records += 1
+    def write(self, value, location):
+        str_val = str(value)
+        if location >= DEFAULT_LOCATION:
+            value_list = self.convert_8byte(str_val)
+            for i in range(len(value_list)):
+                self.data[self.num_records * 8 + i] = value_list[i]
+            self.num_records += 1
+        else : # when it needs to modify the old data
+            value_list = self.convert_8byte(str_val)
+            for i in range(len(value_list)):
+                self.data[location * 8 + i] = value_list[i]
 
     def convert_8byte(self, input):
         hex_list = []
-        hex_input = list(str(input))
+        hex_input = list(input)
         if len(hex_input) % 2 != 0:
             hex_input.insert(0, '0')
 
@@ -34,4 +39,5 @@ class Page:
         for i in range(int(len(hex_input) / 2)):
             temp_str = hex_input[i * 2] + hex_input[i * 2 + 1]
             hex_list.append(int(temp_str))
+        hex_list.append(255)
         return hex_list
--- template/query.py
+++ template/query.py
@@ -26,28 +26,25 @@ class Query:
     # Insert a record with specified columns
     """
 
-    def insert(self, *columns):
-        key = columns[0]  # the first of the column is key from user input
-        rid = key % 906659671
-        schema_encoding = '0' * (self.table.num_columns+5)
-        num_columns = self.table.num_columns+4
-        cur_time = int(time.time()) # unable to store float, so convert to int type
-        indirect = 0
+    def insert(self, *columns, type=TYPE.BASE):
+        if type == TYPE.BASE:
+            key = columns[0]  # the first of the column is key from user input
+            rid = key % 906659671
+            schema_encoding = '0' * (self.table.num_columns+5)
+            num_columns = self.table.num_columns+4
+            cur_time = int(time.time()) # unable to store float, so convert to int type
+            indirect = 0
+            record = Record(rid, key, num_columns, schema_encoding, cur_time, indirect, list(columns[1:]))
+            self.table.write(record)
 
-        # (rid, key, columns, schema_encode, now, indirect, *datas)
-        record = Record(rid, key, num_columns, schema_encoding, cur_time, indirect, list(columns[1:]))
 
-        # print(record.key)
-        # print(record.columns)
-
-        self.table.write(record)
 
     """
     # Read a record with specified key
     """
 
     def select(self, key, query_columns):
-        page_data = self.select(key)
+        page_data = self.select_bytearray(key)
         return page_data
     # FIXME: NEED TO FILTER OUT THE QUERY_COL
 
@@ -55,7 +52,6 @@ class Query:
         rid = self.table.rid_lookup[key]
         page_num = int(rid / NUM_PAGE_RECORDS)
         page = self.table.page_directory[page_num]
-
         record_index = self.table.index_lookup[rid]
         page_data = page.data[record_index*9:record_index*9+72]
         return page_data
@@ -66,20 +62,55 @@ class Query:
     """
 
     def update(self, key, *columns):
-        record = self.select_bytearray(key)
+        # look up the data location
+        rid = self.table.rid_lookup[key]
+        page_num = int(rid / NUM_PAGE_RECORDS)
+        page = self.table.page_directory[page_num]
+        record_index = self.table.index_lookup[rid]
+        record = page.data[record_index*9:record_index*9+72]
+
+        # translate data from bytearray
+        data = self.translate_data(record)
+
+        modify_record = Record(data[1], data[0], data[2], data[3], data[4], data[5], list(columns[1:]))
+        # modify data with key
+        # write(self, record, location=0, type=TYPE.BASE, modify_page=DEFAULT_PAGE):
+        self.table.write(modify_record,record_index, TYPE.TAIL, page_num)
+        # print(record_index)
 
 
-        self.insert(*columns)
+
+    def translate_data(self, data):
+        ret_list = []
+        for i in range(9):
+            record_str = ''
+            for j in range(8):
+                temp_str = str(data[i*8+j])
+                if data[i*8+j] < 10:
+                    record_str += '0' + temp_str
+                elif 255 == data[i*8+j]:
+                    break
+                else:
+                    record_str += temp_str
+            ret_list.append(int(record_str))
+        return ret_list
 
 
-    def get_8byte_data(self, data):
-        ret_str = ''
-        for i in range(len(data)):
-            temp_str = str(data[i])
-            if data[i] < 10:
-                ret_str += '0' + temp_str
-            else :
-                ret_str += temp_str
+        # ret_list = []
+        # ret_str = ''
+        # for i in range(NUM_PAGE_RECORDS):
+        #     temp_str = ''
+        #     for j in range(8):
+        #         temp_str = str(data[i*9+j])
+        #         if data[i] < 10:
+        #             ret_str += '0' + temp_str
+        #         elif data[i] == 255:
+        #             ret_list.append(temp_str)
+        #             break
+        #         else:
+        #             ret_str += temp_str
+        #     ret_list.append(ret_str)
+        # return ret_list
 
     """
     :param start_range: int         # Start of the key range to aggregate 
