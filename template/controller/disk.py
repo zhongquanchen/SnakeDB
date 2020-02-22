2fd4f76e172f937000ef93c6af38d4a3b7744386
djwzg <wangzaige@gmail.com>
2020-02-21 19:03:38 -0800

    Update disk.py

--- template/controller/disk.py
+++ template/controller/disk.py
@@ -1,4 +1,5 @@
 from template.controller.table import Table
+from template.model.page import *
 import os
 from os import path
 
@@ -16,7 +17,7 @@ class disk:
     def readPage(self, pageID)
         filename = str(pageID)
         f = open(filename, 'rb')
-        Page p
+        p = Page()
         p.physical_addr = 0
         p.num_records = 0
         p.data = f.read()
