09e460681dfbb90b854a0e989629afcdeffac8f0
Jerryhe <lolhe@ucdavis.edu>
2020-02-23 00:04:11 -0800

    updated pages

---
+++ lstore/src/copy.py
@@ -0,0 +1,9 @@
+class copy:
+
+    def __init__(self):
+        self.copy = False;
+
+    # copy the base page
+    def copy_base_page(self, old_page, new_page):
+        new_page = old_page
+        return copied_page
